75f324d7093c96e64e5ac491ae4cc7595fb04ad8
thoriqramadhan <respect.thoriq@gmail.com>
2025-01-23 15:24:08 +0700

    feat: change home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 'use client'
-import { useSidebar } from "@/components/context/sidebarContext";
+import { SidebarActivationButton } from "@/components/client/button";
 
 export default function Home() {
-  const { state, setter } = useSidebar()
   return (
     <>
-      <button onClick={() => setter("setStatus", !state)}>OpenClose</button>
       <p>Hello, World!</p>
     </>
   );
