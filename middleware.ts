ab61027221e0873d067aa983c9b864f34a669076
thoriqramadhan <respect.thoriq@gmail.com>
2025-01-23 13:09:06 +0700

    feat: add middleware

---
+++ middleware.ts
@@ -0,0 +1,41 @@
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { decrypt } from './lib/auth/jwt'
+ 
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    const publicRoute = ['/login'];
+    const pathNow = request.nextUrl.pathname;
+    const isInPublicRoute = publicRoute.includes(pathNow)
+    const cookiesStore = await cookies()
+    const jwtSignature = cookiesStore.get('session')?.value
+    let authInfo;
+    try {
+        authInfo = await decrypt(jwtSignature);
+    } catch (error) {
+        authInfo = false;
+    }
+    if (!isInPublicRoute && !authInfo) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl.origin));
+    } else if (isInPublicRoute && authInfo) {
+        return NextResponse.redirect(new URL('/' , request.nextUrl.origin))
+    }
+    console.log(request.nextUrl)
+    return NextResponse.next()
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: [
+    /*
+       * Match all request paths except for:
+       * - API routes
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - Metadata files (favicon.ico, sitemap.xml, robots.txt)
+       * - Static file extensions (e.g., jpg, png, gif, svg, etc.)
+       */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:jpg|jpeg|png|gif|svg|ico|webp|json)).*)',
+    ],
+};
