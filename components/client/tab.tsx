afb770c21b3134b59e0b82917736441df52165f1
thoriqramadhan <respect.thoriq@gmail.com>
2025-01-30 13:33:08 +0700

    feat: add tab components

---
+++ components/client/tab.tsx
@@ -0,0 +1,33 @@
+import { Dispatch, FC, SetStateAction } from 'react';
+
+type tabOption = { name: string }
+interface TabProps {
+    selectedTab: string,
+    settingsOption: tabOption[],
+    setter: Dispatch<SetStateAction<string>>
+}
+
+
+const Tab: FC<TabProps> = ({ selectedTab, settingsOption, setter }) => {
+    return <div className="w-full flex gap-x-3 my-5">
+        {settingsOption.map((option, index) => (
+            <span className={`block px-5 py-2 ${selectedTab == option.name ? 'bg-darkerMain text-white' : 'bg-zinc-50 text-slate-700  hover:bg-zinc-200/50'} rounded-full font-medium cursor-pointer transition-300 capitalize`} key={index} onClick={() => setter(option.name)}>
+                {option.name}
+            </span>
+        ))}
+    </div>
+}
+
+
+interface TabItemProps {
+    name: string
+}
+
+const TabItem: FC<TabItemProps> = ({ name }) => {
+    return <span className={`block px-5 py-2 ${selectedTab == name ? 'bg-main text-white' : 'bg-zinc-50 text-slate-700  hover:bg-zinc-200/50'} rounded-full font-medium cursor-pointer transition-300 capitalize`} key={index} onClick={() => setter(name)}>
+        {name}
+    </span>
+}
+
+
+export default Tab;
