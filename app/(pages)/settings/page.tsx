c6f23691f78c0095328bcae3dc99ed8b5cb290cb
thoriqramadhan <respect.thoriq@gmail.com>
2025-01-30 10:59:53 +0700

    feat: add profile ui in settings

--- app/(pages)/settings/page.tsx
+++ app/(pages)/settings/page.tsx
@@ -1,5 +1,9 @@
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { getAuthInfo } from '@/lib/auth/jwt';
+import _profileSettings from './_settingsComponents/_profileSettings';
 import { FC } from 'react';
+import { boolean } from 'zod';
 
 interface PageProps {
 
@@ -7,9 +11,20 @@ interface PageProps {
 
 const Page: FC<PageProps> = async ({ }) => {
     const authInfo = await getAuthInfo();
-    console.log(authInfo);
-
-    return <div>Page</div>;
+    return <div className='px-5 w-full'>
+        <div className="w-full flex gap-x-3 my-5">
+            {Array.from({ length: 3 }).map((item, index) => (
+                <span className='block bg-zinc-50 px-5 py-2 rounded-full text-slate-700 font-medium cursor-pointer transition-300 hover:bg-black/80 hover:text-white' key={index}>
+                    item
+                </span>
+            ))}
+        </div>
+        <div className="">
+            <h3 className='text-slate-900 font-semibold'>Profile</h3>
+            <p className='text-sm font-thin'>Set yout account detail.</p>
+        </div>
+        <_profileSettings authInfo={authInfo} />
+    </div>;
 }
 
 export default Page;
