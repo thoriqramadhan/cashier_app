779edbb670bfbc1e0bbebf5008d332f60b339192
thoriqramadhan <respect.thoriq@gmail.com>
2025-01-31 09:29:02 +0700

    feat: add logout ui and function

--- components/client/profile.tsx
+++ components/client/profile.tsx
@@ -2,6 +2,8 @@
 import { FC } from 'react';
 import { DropdownContainer, DropdownItem } from './dropdown';
 import { useRouter } from 'next/navigation';
+import { LogOut } from 'lucide-react';
+import { logout } from '@/lib/action/logout';
 
 interface ProfileProps {
 
@@ -12,6 +14,9 @@ const Profile: FC<ProfileProps> = ({ }) => {
     return <DropdownContainer appereance={<div className="h-[36px] w-[36px] bg-zinc-50 rounded-full cursor-pointer"></div>}>
         <DropdownItem onClick={() => router.push('/settings?tab=profile')}>
             Profile
+        </DropdownItem>
+        <DropdownItem icon={<LogOut />} onClick={() => logout()}>
+            Logout
         </DropdownItem>
     </DropdownContainer>
 }
